866c69e6aaeff5ee4d5b2e4483ba3aef63860266
sverevkin25@gmail.com <82106610Vereva>
2019-01-17 18:50:55 +0200

    fix category

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AngularFireDatabase} from '@angular/fire/database';
-import {AngularFirestore} from '@angular/fire/firestore';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +8,19 @@ import {AngularFirestore} from '@angular/fire/firestore';
 export class CategoryService {
 
   constructor(private db: AngularFireDatabase,
-              private afs: AngularFirestore
   ) {
   }
 
   getCategories() {
-    return this.db.list('categories').valueChanges();
+    return this.db.list('categories').snapshotChanges()
+      .pipe(
+        map(actions => {
+          return actions.map(a => {
+            const data = a.payload.val();
+            const id = a.payload.key;
+            return {id, ...data};
+          });
+        })
+      );
   }
 }
