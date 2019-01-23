6762006b8f54dee5a8ce9ade8f5fe78675ea62ae
Stanislav Verovkin <sverevkin25@gmail.com>
2019-01-23 09:21:45 +0200

    design responsive

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -2,12 +2,14 @@ import {Injectable} from '@angular/core';
 import {CanActivate, Router} from '@angular/router';
 import {AuthService} from './auth.service';
 import {map} from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) {
+  constructor(private auth: AuthService, private router: Router, private snackBar: MatSnackBar
+  ) {
 
   }
 
@@ -18,6 +20,9 @@ export class AuthGuard implements CanActivate {
           return true;
         }
         this.router.navigate(['/login']);
+        this.snackBar.open('Please Join us to order products', 'Close' , {
+          duration: 5000
+        });
         return false;
       })
     );
