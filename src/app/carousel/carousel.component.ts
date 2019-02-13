cc434c28e1c4c38c9bd66f7e830d28f807b330c9
Stanislav Verovkin <sverevkin25@gmail.com>
2019-02-13 09:32:29 +0200

    new design

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -3,6 +3,8 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss'],
   animations: [
     trigger('thumbState', [
       state('inactive', style({
@@ -15,35 +17,12 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       transition('active => inactive', animate('500ms cubic-bezier(0.785, 0.135, 0.15, 0.86)'))
     ])
   ],
-  styles: [`
-    .carousel {
-      display: flex;
-      justify-content: center;
-      width: 100%;
-      align-items: center;
-    }
-
-    .tmb img {
-      width: 400px;
-      cursor: pointer;
-      box-shadow: 0 0 35px rgba(0, 0, 0, 0.5);
-      margin: -200px;
-    }
-  `],
-  template: `
-    <div class="carousel">
-      <div [@thumbState]="idx === counter ? 'active' : 'inactive'" class="tmb"
-           *ngFor="let img of images; let idx = index">
-        <img [src]="img" (click)="onClickThumb($event)"/>
-      </div>
-    </div>
-  `
 })
 
 export class CarouselComponent {
   @Input() images: Array<string>;
   @Output() change: EventEmitter<number> = new EventEmitter<number>();
-  counter = 1;
+  counter = 0;
 
   onClickThumb(event) {
     const total = this.images.length - 1;
